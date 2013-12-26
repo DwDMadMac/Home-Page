47ce693db1848b84ab108e288791128029f34985
MadMac MacAllister <madmac@PrimLinux.downwithdestruction.net>
2013-12-26 06:20:52 -0500

    Removed unused files.

--- prepend.php
+++
@@ -1,9 +0,0 @@
-<?php
-    define('MAINDIR',dirname(__FILE__) . '/'); //define main directory path
-    define('CSSPATH', MAINDIR . 'css/'); //define css path
-    define('INCPATH', MAINDIR . 'inc/'); //define inc path
-
-    $cssMain = 'mainDev.css'; //css item to display
-    $incHeader = 'headerDev.php'; //inc page to display
-?>
-
